534f52f50a22dc1c5c45ae234526263d02bd66cc
cristianmino <cristianeisasa@gmail.com>
2023-01-18 14:24:27 -0300

    Resolviendo el problema del ejercicio

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
+
+  let [reflect, setReflect] = useState("");
+
+  let reflectText = (e) =>{
+    setReflect(
+      reflect = e.target.value
+    )
+  }
+
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo"  onChange={reflectText} />
+        <p className="repeater">{reflect}</p>
       </div>
     );
   }
-}
 
 export default App;
